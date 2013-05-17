2f4dcabc1d9ac32ea900661f18e0339be650c2eb
Jared King <jared@nfuseweb.com>
2013-05-17 11:31:52 -0500

    documented router

--- libs/Router.php
+++ libs/Router.php
@@ -1,9 +1,36 @@
 <?php
+/**
+ * @package nFuse
+ * @author Jared King <j@jaredtking.com>
+ * @link http://jaredtking.com
+ * @version 1.0
+ * @copyright 2013 Jared King
+ * @license MIT
+	Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+	associated documentation files (the "Software"), to deal in the Software without restriction,
+	including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+	and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+	subject to the following conditions:
+	
+	The above copyright notice and this permission notice shall be included in all copies or
+	substantial portions of the Software.
+	
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+	LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+	IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+	WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+	SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 
 namespace nfuse;
 
 class Router
 {
+	/**
+	 * @staticvar $apiRoutes
+	 *
+	 * Default routes for modules with automatic api generation enabled
+	 */
 	private static $apiRoutes = array(
 		'get /:controller' => 'findAll',
 		'get /:controller/:id' => 'find',
@@ -12,6 +39,12 @@ class Router
 		'delete /:controller/:id' => 'delete'
 	);
 	
+	/**
+	 * Routes a request and resopnse to the appropriate controller. Sends a 404 if nothing was found.
+	 *
+	 * @param Request $req
+	 * @param Response $res
+	 */
 	static function route( $req, $res )
 	{
 		/*
