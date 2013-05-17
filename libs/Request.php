d0c84d9ebc66499ffc51fff47f1dd72ac4ed6886
Jared King <jared@nfuseweb.com>
2013-05-17 13:22:41 -0500

    added multipart/form-data parsing to requests

--- libs/Request.php
+++ libs/Request.php
@@ -106,22 +106,26 @@ class Request
 		
 		$this->setPath( val( $this->server, 'REQUEST_URI' ) );
 			
-		// get the request method
+		$this->request = array();
+		
 		if( $request )
 			$this->request = $request;
 		// decode request body for POST and PUT
 		else if( in_array( $this->method(), array( 'POST', 'PUT' ) ) )
 		{
-			$request = file_get_contents( 'php://input' );
+			$body = file_get_contents( 'php://input' );
+			$contentType = $this->contentType();
 			
 			// parse json
-			if( strpos( $this->contentType(), 'application/json') !== false )
-				$this->request = json_decode( $request, true );
+			if( strpos( $contentType, 'application/json') !== false )
+				$this->request = json_decode( $body, true );
+			// parse multipart form data
+			else if( strpos( $contentType, 'multipart/form-data' ) !== false )
+				$this->request = $_REQUEST;
 			// parse query string
 			else
-				parse_str( $request, $this->request );
-		} else
-			$this->request = array();			
+				parse_str( $body, $this->request );
+		}
 	}
 	
 	/** 
@@ -600,5 +604,64 @@ class Request
 		} else {
 			return -1;
 		}
-	}	
+	}
+	
+	/**
+	 * Credit to http://www.chlab.ch/blog/archives/php/manually-parse-raw-http-data-php
+	 * Parse raw HTTP request data
+	 *
+	 * Pass in $a_data as an array. This is done by reference to avoid copying
+	 * the data around too much.
+	 *
+	 * Any files found in the request will be added by their field name to the
+	 * $data['files'] array.
+	 *
+	 * @param	string	Input request
+	 * @param	string	Content type
+	 * @param   array	Empty array to fill with data
+	 * @return  array	Associative array of request data
+	 */
+	private function parse_raw_http_request($input, $contentType, array &$a_data)
+	{
+	  // grab multipart boundary from content type header
+	  preg_match('/boundary=(.*)$/', $contentType, $matches);
+	   
+	  // content type is probably regular form-encoded
+	  if (!count($matches))
+	  {
+	    // we expect regular puts to containt a query string containing data
+	    parse_str(urldecode($input), $a_data);
+	    return $a_data;
+	  }
+	   
+	  $boundary = $matches[1];
+	 
+	  // split content by boundary and get rid of last -- element
+	  $a_blocks = preg_split("/-+$boundary/", $input);
+	  array_pop($a_blocks);
+	       
+	  // loop data blocks
+	  foreach ($a_blocks as $id => $block)
+	  {
+	    if (empty($block))
+	      continue;
+	     
+	    // you'll have to var_dump $block to understand this and maybe replace \n or \r with a visibile char
+	     
+	    // parse uploaded files
+	    if (strpos($block, 'application/octet-stream') !== FALSE)
+	    {
+	      // match "name", then everything after "stream" (optional) except for prepending newlines
+	      preg_match("/name=\"([^\"]*)\".*stream[\n|\r]+([^\n\r].*)?$/s", $block, $matches);
+	      $a_data['files'][$matches[1]] = $matches[2];
+	    }
+	    // parse all other fields
+	    else
+	    {
+	      // match "name" and optional value in between newline sequences
+	      preg_match('/name=\"([^\"]*)\"[\n|\r]+([^\n\r].*)?\r$/s', $block, $matches);
+	      $a_data[$matches[1]] = $matches[2];
+	    }
+	  }
+	}
 }
