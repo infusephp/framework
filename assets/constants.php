881d9eb1e87c5d7c30de7130f73d93ce81798a2f
Jared King <j@jaredtking.com>
2014-08-25 11:02:28 -0500

    added constants.php

---
+++ assets/constants.php
@@ -0,0 +1,5 @@
+<?php
+
+// define any custom constants specific to your app here
+// i.e. define( 'POINTS_QUEUE_NAME', 'points' );
+
--- public/index.php
+++ public/index.php
@@ -13,6 +13,7 @@ define( 'INFUSE_BASE_DIR', dirname( __DIR__ ) );
 set_include_path( get_include_path() . PATH_SEPARATOR . INFUSE_BASE_DIR );
 
 require_once 'vendor/autoload.php';
+include 'assets/constants.php';
 
 $config = @include 'config.php';
 $app = new App( $config );
