5383c6d2f9cfe42125a5c29c28090e8f1c423ce5
Jared King <j@jaredtking.com>
2014-10-10 09:09:08 -0500

    support improved phinx migrations from bootstrap

--- phinx.php
+++ phinx.php
@@ -9,36 +9,24 @@
  * @license MIT
  */
 
-require_once 'vendor/autoload.php';
-
 $phinxConfig = [
-    'paths' => [],
     'environments' => [
         'default_migration_table' => 'Migrations',
         'default_database' => 'app' ] ];
 
 $appConfig = @include 'config.php';
 
-// determine the module's path
-$module = getenv( 'PHINX_APP_MODULE' );
-if ($module) {
-    // determine module directory
-    $controller = '\\app\\' . $module . '\\Controller';
+$migrationPath = getenv('PHINX_MIGRATION_PATH');
+if ($migrationPath)
+    $phinxConfig['paths'] = ['migrations' => $migrationPath];
 
-    if ( class_exists( $controller ) ) {
-        $reflection = new ReflectionClass( $controller );
-        $modDir = dirname( $reflection->getFileName() ) . '/migrations';
-        $phinxConfig[ 'paths' ][ 'migrations' ] = $modDir;
-    }
-}
+// generate database environment from config
+$environment = $appConfig['database'];
+$environment['adapter'] = $environment['type'];
+unset($environment['type']);
+$environment['pass'] = $environment['password'];
+unset($environment['password']);
 
-// generate environment from config
-$environment = $appConfig[ 'database' ];
-$environment[ 'adapter' ] = $environment[ 'type' ];
-unset( $environment[ 'type' ] );
-$environment[ 'pass' ] = $environment[ 'password' ];
-unset( $environment[ 'password' ] );
-
-$phinxConfig[ 'environments' ][ 'app' ] = $environment;
+$phinxConfig['environments']['app'] = $environment;
 
 return $phinxConfig;
